94b3f041d77be7c42414805412a8a0a6dde0002d
David Rodriguez <davidrodrigueznolimit@gmail.com>
2023-07-03 22:40:09 -0500

    updates testing and test logic

--- cypress/e2e/quiz.cy.js
+++ cypress/e2e/quiz.cy.js
@@ -1,7 +1,7 @@
 Cypress._.times(100, () => {
   describe("Landing page checks", () => {
     it("lands on the home page, verifies the carousel is working, and clicks the Start Quiz button", () => {
-      cy.visit("http://localhost:8080");
+      cy.visit("http://localhost:5500");
       cy.get("#landing > a > #startQuiz").click();
 
       // create a random number and assign it to a variable so the answers selected are semi randomized
